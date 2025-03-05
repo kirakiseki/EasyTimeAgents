d4feb89909a2cbcbfdd7e780a1d5083167c7dc85
kirakiseki <kirakiseki@163.com>
2025-03-06 03:06:57 +0800

    feat: add evaluator/worker/visualizer agent

--- src/agents/agents/agents.py
+++ src/agents/agents/agents.py
@@ -1,7 +1,7 @@
-from gradio.server_messages import BaseMessage
 from langchain_core.language_models import BaseChatModel
 from langchain_core.runnables import RunnableSerializable
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import BaseMessage
 
 from . import prompts
 
@@ -31,3 +31,35 @@ def get_decomposer_agent(
     model: BaseChatModel,
 ) -> RunnableSerializable[dict, BaseMessage]:
     return _get_agent(model, prompts.DECOMPOSER_SYSTEM_INSTRUCTION)
+
+
+def get_evaluator_agent(
+    model: BaseChatModel,
+) -> RunnableSerializable[dict, BaseMessage]:
+    prompt = ChatPromptTemplate(
+        [
+            ("placeholder", "{messages}"),
+            ("system", prompts.EVALUATOR_SYSTEM_INSTRUCTION),
+        ]
+    )
+
+    return prompt | model
+
+
+def get_worker_agent(model: BaseChatModel) -> RunnableSerializable[dict, BaseMessage]:
+    model_with_stop = model.bind(stop=["\nObservation"])
+
+    prompt = ChatPromptTemplate(
+        [
+            ("placeholder", "{messages}"),
+            ("system", prompts.WORKER_SYSTEM_INSTRUCTION),
+        ]
+    )
+
+    return prompt | model_with_stop
+
+
+def get_visualizer_agent(
+    model: BaseChatModel,
+) -> RunnableSerializable[dict, BaseMessage]:
+    return _get_agent(model, prompts.VISUALIZER_SYSTEM_INSTRUCTION)
